2382b01d6060037838229e695aec33a6057097de
Eyobel Legese <eyobel7k@gmail.com>
2021-11-29 12:44:40 -0800

    deleted old ReadMe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const[listContainer, setListContainer]= useState([]);
+  const [listContainer, setListContainer]= useState([]);
   return (
     <div className="App">
       <header className="App-header">
