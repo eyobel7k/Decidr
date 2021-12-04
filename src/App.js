9ae10a3aad88c78c3a82c67bfc24cd50b7e22d9d
Eyobel Legese <eyobel7k@gmail.com>
2021-12-04 14:56:34 -0800

    feat(App.js) add Display list and Add Item components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
+import AddItem from "./Components/AddItem";
+import DisplayList from "./Components/DisplayList";
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Petemoss&display=swap');
+</style>;
 
 function App() {
-  const [listContainer, setListContainer]= useState([]);
+  const [listContainer, setListContainer] = useState([]);
+  const [selectedItem, setSelectedItem] = useState("");
+  const [count, setCount] = useState(0);
+  const [errorWarning, setErrorWarning] = useState(" Add At Least Two Items");
+  const [itemWarning, setItemWarning] = useState("Add At Least Two Items");
+
+  const selectItem = () => {
+    const random = Math.floor(Math.random() * listContainer.length);
+    setSelectedItem(
+      listContainer.length > 1 ? listContainer[random] : errorWarning
+    );
+
+    setItemWarning("");
+  };
+
+  const completeTask = (index) => {
+    let copyOfArray = [...listContainer];
+    copyOfArray.splice(index, 1);
+    setListContainer(copyOfArray);
+    setCount(count - 1);
+  };
+  const clearAll = (index) => {
+    let copyOfArray = [...listContainer];
+    copyOfArray.splice(index);
+    setListContainer(copyOfArray);
+    setSelectedItem("");
+    setCount(0);
+    setItemWarning(errorWarning);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>DECIDR</h1>
       </header>
+
+      <AddItem
+        listContainer={listContainer}
+        setListContainer={setListContainer}
+        completeTask={completeTask}
+        setCount={setCount}
+        count={count}
+      />
+
+      <main>
+        {listContainer.length < 2 && (
+          <div className="item-warning">{itemWarning}</div>
+        )}
+
+        {selectedItem.length > 0 ? (
+          <p id="selected"> {selectedItem} </p>
+        ) : (
+          <DisplayList
+            listContainer={listContainer}
+            setListContainer={setListContainer}
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+            setCount={setCount}
+            completeTask={completeTask}
+          />
+        )}
+      </main>
+      <footer>
+        <div className="btn">
+          <button id="btn-select" onClick={() => selectItem()} type="text">
+            Selected Item
+          </button>
+          <button onClick={() => clearAll()} type="text">
+            Clear All
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
